081520dc708f8d1ae6deade6b37fdf9106ef7b66
Isra Lamrani <lrn.arsi@gmail.com>
2025-03-07 01:46:58 -0500

    Update Hero.jsx

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -1,35 +1,48 @@
 import React from 'react';
 import './hero.css';
 
+
 const Hero = () => {
   return (
-    <div className='hero' id='Hero'>
+    <div className='hero ' id='Hero'>
+
       {/* my header */}
-      <div className='header'>
+
+      <div className='header ' >
         {/* My logo */}
         <h1 className='logo'>IsraLamrani</h1>
 
-        <div className='icons'>
+        <div className='icons '>
           {/* socialLinks */}
-          <div className='socialLinks'>
+          <div className='socialLinks '>
             <ul>
               <li>
-                <a href="https://github.com/Lamisra" target="_blank" rel="noopener noreferrer">
-                  <img src={require('../assets/images/icon-github.svg')} alt="github icon" />
+                <a href="https://github.com/Lamisra" target="_blank">
+                  <img src="../src/assets/images/icon-github.svg" alt="github icon" />
                 </a>
               </li>
               <li>
-                <a href="https://www.linkedin.com/in/isra-lamrani/" target="_blank" rel="noopener noreferrer">
-                  <img src={require('../assets/images/icon-linkedin.svg')} alt="linkedin icon" />
+                <a href="https://www.linkedin.com/in/isra-lamrani/" target="_blank">
+                  <img src="src\assets\images\icon-linkedin.svg" alt="linkedin icon" />
                 </a>
               </li>
             </ul>
           </div>
+
+          {/* light button */}
+          {/* <div className='lightButton '>
+            
+          </div> */}
+
         </div>
+
+
+
       </div>
 
+
       {/* my main page */}
-      <img className='profilePicture' src={require('../assets/images/profile-pic.png')} alt='profile picture' />
+      <img className='profilePicture' src='../src/assets/images/profile-pic.png' alt='profile picture' />
 
       <h2 className='xl'>
         Nice to meet you! <br />
@@ -44,9 +57,19 @@ const Hero = () => {
 
       <a className='button' href='#contact'>CONTACT ME</a>
 
-      <div className='circle'></div>
+      <div className='circle' >
+
+      </div>
+
+
+
+
+
+      
+
+
     </div>
-  );
+  )
 }
 
 export default Hero;
