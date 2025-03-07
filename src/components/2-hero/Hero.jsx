609bc7a3dceaabb9643752c32c80f4d859ca81c3
Isra Lamrani <lrn.arsi@gmail.com>
2025-03-07 01:22:35 -0500

    Update Hero.jsx

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -1,59 +1,41 @@
 import React from 'react';
 import './hero.css';
 
-
 const Hero = () => {
   return (
-    <div className='hero ' id='Hero'>
-
+    <div className='hero' id='Hero'>
       {/* my header */}
-
-      <div className='header ' >
+      <div className='header'>
         {/* My logo */}
         <h1 className='logo'>IsraLamrani</h1>
 
-        <div className='icons '>
+        <div className='icons'>
           {/* socialLinks */}
-          <div className='socialLinks '>
+          <div className='socialLinks'>
             <ul>
               <li>
-                <a href="https://github.com/Lamisra" target="_blank">
-                  {/* <img src="./src/assets/images/icon-github.svg" alt="github icon" /> */}
-                  <img src={require('./assets/images/icon-github.svg')} alt="github icon" />
-
+                <a href="https://github.com/Lamisra" target="_blank" rel="noopener noreferrer">
+                  <img src={require('../assets/images/icon-github.svg')} alt="github icon" />
                 </a>
               </li>
               <li>
-                <a href="https://www.linkedin.com/in/isra-lamrani/" target="_blank">
-                  <img src="src\assets\images\icon-linkedin.svg" alt="linkedin icon" />
+                <a href="https://www.linkedin.com/in/isra-lamrani/" target="_blank" rel="noopener noreferrer">
+                  <img src={require('../assets/images/icon-linkedin.svg')} alt="linkedin icon" />
                 </a>
               </li>
             </ul>
           </div>
-
-          {/* light button */}
-          {/* <div className='lightButton '>
-            
-          </div> */}
-
         </div>
-
-
-
       </div>
 
-
       {/* my main page */}
-      {/* <img className='profilePicture' src='../assets/images/profile-pic.png' alt='profile picture' /> */}
       <img className='profilePicture' src={require('../assets/images/profile-pic.png')} alt='profile picture' />
 
-
       <h2 className='xl'>
-        oice to meet you! <br />
+        Nice to meet you! <br />
         I'm <span>Isra Lamrani</span>.
       </h2>
-<img src=".../cat.jpeg">
-      
+
       <p className='p'>
         Based in Montreal, I'm a front-end developer<br />
         passionate about building accessible web apps
@@ -62,19 +44,9 @@ const Hero = () => {
 
       <a className='button' href='#contact'>CONTACT ME</a>
 
-      <div className='circle' >
-
-      </div>
-
-
-
-
-
-      
-
-
+      <div className='circle'></div>
     </div>
-  )
+  );
 }
 
 export default Hero;
